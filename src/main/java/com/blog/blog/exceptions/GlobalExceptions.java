e5a078b190e297abd2939fe189c73176d588d0a2
Varsani2520 <varsaniranjani@gmail.com>
2025-03-11 10:14:23 +0530

    global exceptions

--- src/main/java/com/blog/blog/exceptions/GlobalExceptions.java
+++ src/main/java/com/blog/blog/exceptions/GlobalExceptions.java
@@ -1,8 +1,12 @@
 package com.blog.blog.exceptions;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,10 +14,22 @@ import com.blog.blog.payload.ApiResponse;
 
 @RestControllerAdvice
 public class GlobalExceptions {
+
+    // handling resource not found exceptions
     @ExceptionHandler(ResourceNotFound.class)
     public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFound e) {
         String msg = e.getMessage();
         ApiResponse res = new ApiResponse(msg, false);
         return new ResponseEntity<ApiResponse>(res, HttpStatus.NOT_FOUND);
+    }
+
+     // Handling Validation Errors
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach((error) -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
