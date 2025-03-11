e56f8ee042a0bbe176f1ce76b77e42357fd59e6e
Varsani2520 <varsaniranjani@gmail.com>
2025-03-11 10:11:59 +0530

    initialize blog application

--- src/main/java/com/blog/blog/BlogApplication.java
+++ src/main/java/com/blog/blog/BlogApplication.java
@@ -1,13 +1,25 @@
 package com.blog.blog;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.blog.blog.Entities.User;
+import com.blog.blog.payload.UserDTO;
 
 @SpringBootApplication
 public class BlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelmapper(){
+		ModelMapper modelmapper=new ModelMapper();
+		modelmapper.typeMap(UserDTO.class, User.class).addMappings(mapper->mapper.skip(User::setId));
+		return modelmapper;
+
 	}
 
 }
